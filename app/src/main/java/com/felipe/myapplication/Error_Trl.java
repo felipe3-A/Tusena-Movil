3d93c7b1cdf868a7dfa7d0d1fb1f060bfad2eed6
felipe3-A <lenovo.esucre@gmail.com>
2023-05-26 13:26:58 -0500

    Trls con identi

--- app/src/main/java/com/felipe/myapplication/Error_Trl.java
+++ app/src/main/java/com/felipe/myapplication/Error_Trl.java
@@ -3,12 +3,23 @@ package com.felipe.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Error_Trl extends AppCompatActivity {
-
+TextView respuestas,respuestas_trl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error_trl);
+        referenciar();
+    }
+
+    private void referenciar() {
+        respuestas=findViewById(R.id.resultado);
+        respuestas_trl=findViewById(R.id.resultado_trl);
+
+        respuestas.setText("Su porcentaje es  : "+Tlr1_1.todos + "%");
+        respuestas_trl.setText("Su Trl es : "+Tlr1_1.nivel);
+
     }
 }
--- app/src/main/java/com/felipe/myapplication/Tlr1_1.java
+++ app/src/main/java/com/felipe/myapplication/Tlr1_1.java
@@ -307,18 +307,10 @@ public class Tlr1_1 extends AppCompatActivity {
                     public void onClick(View view) {
                         todos=resultado+resultadop2+resultadop3+resultadop4+resultadop5+resultadop6+resultadop_7;
 
-<<<<<<< HEAD
-                        if(todos>= 100){
-                            nivel= "Trl2";
-                            Intent intent = new Intent(Tlr1_1.this,Trl2.class);
-                            startActivity(intent);
-                        }
-=======
->>>>>>> 084ffec72e577649eee33501e5a4428c4d9846ec
 
 
                         if(todos>= 100) {
-                            nivel = "Tlr2";
+                            //nivel = "Tlr2";
 
                                 Intent intent = new Intent(Tlr1_1.this, Trl2.class);
                                 startActivity(intent);
@@ -326,9 +318,11 @@ public class Tlr1_1 extends AppCompatActivity {
 
                         }
                        else{
-                            Intent intent = new Intent(Tlr1_1.this,Login.class);
+                            nivel = "Tlr1";
+                            Intent intent = new Intent(Tlr1_1.this, Error_Trl.class);
                             startActivity(intent);
-                            Toast.makeText(Tlr1_1.this, "sus resultados "+ todos +"%", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Tlr1_1.this, "s" +
+                                    "us resultados "+ todos +"%", Toast.LENGTH_SHORT).show();
                         }
 
 
